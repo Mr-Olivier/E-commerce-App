621b028076a3fd86534b7424dd0dbbdd2990b6b3
Olivier <iradukundaolivier2000@gmail.com>
2024-11-19 00:26:59 +0200

    feat: first commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express, { Express, Request, Response } from "express";
+import { PORT } from "./secrets";
+import rootRouter from "./routes";
+import { PrismaClient } from "@prisma/client";
+
+const app: Express = express();
+
+app.use(express.json());
+
+app.use("/api", rootRouter);
+
+export const prisma = new PrismaClient({
+  log: ["query"],
+});
+
+app.listen(PORT, () => {
+  console.log("App working!!!!");
+});
+
+// localhost:4000/api/auth/login
